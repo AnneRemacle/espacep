7c8fe70bbd142d3d7e3d5899844d2f0101593ba0
Anne Remacle <anne.m.remacle@gmail.com>
2017-08-05 08:56:47 +0200

    Main page with php

--- dev/wp-content/themes/espacep/index.php
+++ dev/wp-content/themes/espacep/index.php
@@ -1,49 +1,54 @@
 <?php
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * e.g., it puts together the home page when no home.php file exists.
+ * Template Name: Homepage
  */
 get_header();?>
 
-<section class="section news">
-	<figure class="news__figure">
-		<img src="" alt="" class="news__figure--img" />
-	</figure>
-	<div class="news__infos">
-		<p class="news__infos--category colored">Santé</p>
-		<h2 class="news__infos--title">Je suis le titre</h2>
-		<p class="news__infos--summary">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-		<a href="" class="news__infos--link colored">Lire l'article <span class="sro">titre de l'article</span></a>
-	</div>
-</section>
+<div class="section news">
+	<?php $posts = new WP_Query( [ 'orderby' => 'date', 'posts_per_page' => 1, 'order' => 'ASC' ] ); ?>
+		<?php if ( $posts -> have_posts() ):
+			while ( $posts -> have_posts() ):
+				$posts -> the_post(); ?>
+
+		<article class="article">
+
+			<figure class="article__figure">
+				<?php the_post_thumbnail(medium, ['class' => 'article__figure--image', 'alt' => 'Image de l‘article']);?>
+			</figure>
+			<div class="news__infos">
+				<p class="news__infos--category colored">Santé</p>
+				<h2 class="article__title" role="heading" aria-level="2"><?php the_title(); ?> </h2>
+				<p class="news__infos--summary"><?php the_custom_excerpt(); ?>…</p>
+				<a href="<?php the_permalink(); ?>" class="news__infos--link colored">Lire l'article <span class="sro"><?php the_title(); ?> en entier</span></a>
+			</div>
+		</article>
+
+	<?php endwhile; endif; ?>
+	<?php wp_reset_query(); ?>
+</div>
 
 <section class="imaged support">
-	<h2 class="imaged__title">Soutenez-nous en vous abonnant à notre page Facebook</h2>
-	<a href="" class="imaged__link button">Abonnez-vous</a>
+	<h2 class="imaged__title"><?php the_field('section_title'); ?></h2>
+	<a href="" class="imaged__link button"><?php the_field('link_text'); ?></a>
 </section>
 
 <section class="section last">
 	<h2 class="section__title">Dernièrement chez Espace P…</h2>
 	<div class="last__facebook">
-		<p>Statut Facebook</p>
+		<?php the_field('facebook_feed'); ?>
 	</div>
 </section>
 
 <div class="frames section">
-	<section class="frames__section">
-		<h2 class="section__title">Signez notre manifeste</h2>
-		<p class="frames__section--text">Pour une approche plus juste des métiers du sexe</p>
-		<a href="" class="frames__section--link button">Lire et signer</a>
-	</section>
-	<section class="frames__section">
-		<h2 class="section__title">Abonnez-vous au magazine</h2>
-		<p class="frames__section--text">Nous éditons un magazine destiné aux travailleurs du sexe ainsi qu’à nos sympathisants et aux personnes qui souhaitent s’informer.</p>
-		<a href="" class="frames__section--link button">S"abonner</a>
-	</section>
+	<?php if( have_rows('frames') ):
+		while ( have_rows('frames') ) : the_row();?>
+		<section class="frames__section">
+			<h2 class="section__title"><?php the_sub_field('title'); ?></h2>
+			<p class="frames__section--text"><?php the_sub_field('text'); ?></p>
+			<a href="" class="frames__section--link button"><?php the_sub_field('link_text'); ?></a>
+		</section>
+		<?php endwhile; ?>
+	<?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
