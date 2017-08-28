24d7ab0f64698a6160ba04182552b577ea59a995
Anne Remacle <anne.m.remacle@gmail.com>
2017-08-28 16:01:34 +0200

    Page 404

--- dev/wp-content/themes/espacep/404.php
+++ dev/wp-content/themes/espacep/404.php
@@ -6,22 +6,16 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<section class="section white-pattern">
+		<h1 class="section__title"><?php _e( 'Oups&nbsp;! Cette page est introuvable.', 'espacep' ); ?></h1>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyfifteen' ); ?></h1>
-				</header><!-- .page-header -->
+		<div class="page-content">
+			<p><?php _e( 'Rien n’a été trouvé à cette adresse, essayez peut-être de faire une recherche', 'espacep' ); ?></p>
 
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
+			<div class="search__failed">
+				<?php get_search_form(); ?>
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
